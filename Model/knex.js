752e4ea5a948f35de3b02d6b4aa3004a8865969d
zebanavgurukul <zeba18@navgurukul.org>
2020-04-11 15:09:33 +0200

    commit

--- Model/knex.js
+++ Model/knex.js
@@ -1,24 +1,24 @@
 const knex=require("./connection")
 
-
 let register = (data)=>{
     return knex('register').insert({Username:data.Username ,email:data.email ,password:data.password})
 }
 
 let login = (email)=>{
     return knex.select("*").from('register').havingIn('register.email',email)
-
 }
 
+let logindata = (password) => {
+    return knex.select('*').from('register').havingIn('register.password',password)
+}
 
-
-
+let updateData = (update) => {
+    return knex('register').update({password:update.newpassword}).where('register.user_id', update.user_id)
+}
 
 
 let Bio = (Bio_data) => {
     return knex('Bio_detail').insert({'data_brith':Bio_data.data_brith,"user_id":Bio_data.user_id,"email":Bio_data.email,"likes":Bio_data.phone_number})
 }
 
-
-
-module.exports={register, login , Bio};
+module.exports={register, login , Bio , logindata, updateData};
--- Routes/login.js
+++ Routes/login.js
@@ -39,6 +39,34 @@ module.exports=(app,jwt)=>{
         })
     })
     
+    app.put('/update',(req,res) => {
+        let password = req.query.password
+        let new_password = req.query.new_password
+        let alltoken = req.headers.cookie
+        var token = alltoken.split('=')
+        token = (token[token.length-2]).slice(11,300)
+        jwt.verify(token, 'zeba', (err,user_detail) => {
+            var data = user_detail['jwt']
+            var user_id = data[0]['user_id']
+            console.log(user_id)
+            knex.logindata(password)
+            .then((data)=>{
+                if(data[0]['password'] == password){
+                    let update = {
+                        user_id : user_id,
+                        newpassword : new_password
+                    }
+                    knex.updateData(update)
+                    .then(()=>{
+                        res.send('update...')
+                    })
+                }else{
+                    res.send('you password in invalid')
+                }
+            })
+        }) 
+    })
+
     app.get('/verify',(req,res)=>{ 
         var token = req.query.token;
         jwt.verify(token, 'aadil', (err)=>{
